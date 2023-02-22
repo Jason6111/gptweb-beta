950c689f50bc4910322ed689ab1eab1e104ac105
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-02-22 22:02:47 +0800

    bate

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -2,6 +2,6 @@
 
 interface ImportMetaEnv {
 	readonly VITE_GLOB_API_URL: string;
-  readonly VITE_GLOB_API_TIMEOUT: string;
+	readonly VITE_GLOB_API_TIMEOUT: string;
 	readonly VITE_GLOB_HTTP_PROXY: 'Y' | 'N';
 }
