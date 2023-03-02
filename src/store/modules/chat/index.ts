b032d2655ec060a8647ddeeed75541a27667093b
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-03-02 18:51:29 +0800

    Update index.ts

--- src/store/modules/chat/index.ts
+++ src/store/modules/chat/index.ts
@@ -104,18 +104,6 @@ export const useChatStore = defineStore('chat-store', {
         }
       }
 
-      const language = navigator.language.toLowerCase();
-      if (language.includes('zh')) {
-        // 如果系统语言是中文，将标题改为“新聊天”
-        if (this.history[index].title === 'New Chat') {
-          this.history[index].title = '新聊天';
-        }
-      } else {
-        // 如果系统语言是英文或其他语言，将标题改为“New Chat”
-        if (this.history[index].title === '新聊天') {
-          this.history[index].title = 'New Chat';
-        }
-      },
       const index = this.chat.findIndex(item => item.uuid === uuid)
       if (index !== -1) {
         this.chat[index].data.push(chat)
