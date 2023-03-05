31bc42b2a9a4e05d39c69edf00beea92543afa02
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-03-05 14:21:49 +0800

    复原

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,8 +1,8 @@
 import type { App } from 'vue'
 import setCopyDirective from './copy'
-// import setupHighlightDirective from './highlight'
+import setupHighlightDirective from './highlight'
 
 export function setupDirectives(app: App) {
   setCopyDirective(app)
-  // setupHighlightDirective(app)
+  setupHighlightDirective(app)
 }
--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 import { computed, ref } from 'vue'
 import MarkdownIt from 'markdown-it'
+import hljs from 'highlight.js'
 import mdKatex from '@traptitech/markdown-it-katex'
-import mdhljs from 'markdown-it-highlightjs'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
 
 interface Props {
@@ -16,15 +16,73 @@ const props = defineProps<Props>()
 
 const { isMobile } = useBasicLayout()
 
-const textRef = ref<HTMLElement>()
-
 const mdi = new MarkdownIt({
   linkify: true,
+  highlight: (code, language) => {
+    const validLang = !!(language && hljs.getLanguage(language))
+    if (validLang) {
+      const lang = language ?? ''
+      return `<pre><code class="hljs ${language}">${hljs.highlight(code, { language: lang }).value}</code></pre>`
+    }
+    return `<pre style="background: none">${hljs.highlightAuto(code).value}</pre>`
+  },
 })
 
-mdi
-  .use(mdhljs, { auto: true, inline: true })
-  .use(mdKatex, { blockClass: 'katexmath-block rounded-md p-[10px]', errorColor: ' #cc0000' })
+mdi.use(mdKatex, { blockClass: 'katexmath-block', errorColor: ' #cc0000' })
+
+const textRef = ref<HTMLElement>()
+
+// renderer.html = (html) => {
+//   return `<p>${encodeHTML(html)}</p>`
+// }
+
+// const katexOptions = {
+//   throwOnError: false,
+// }
+
+// const katexInline = {
+//   name: 'katexInline',
+//   level: 'inline',
+//   start(src: string) {
+//     return src.indexOf('$')
+//   },
+//   tokenizer(src: string) {
+//     const match = src.match(/^\$+([^$\n]+?)\$+/)
+//     if (match) {
+//       return {
+//         type: 'katexInline',
+//         raw: match[0],
+//         text: match[1].trim(),
+//       }
+//     }
+//   },
+//   renderer(token: marked.Tokens.Generic) {
+//     return katex.renderToString(token.text, katexOptions)
+//   },
+// // }
+
+// const katexBlock = {
+//   name: 'katexBlock',
+//   level: 'block',
+//   start(src: string) {
+//     return src.indexOf('\n$$')
+//   },
+//   tokenizer(src: string) {
+//     const match = src.match(/^\$\$+\n([^$]+?)\n\$\$+\n/)
+//     if (match) {
+//       return {
+//         type: 'katexBlock',
+//         raw: match[0],
+//         text: match[1].trim(),
+//       }
+//     }
+//   },
+//   renderer(token: marked.Tokens.Generic) {
+//     return `<p>${katex.renderToString(token.text, katexOptions)}</p>`
+//   },
+// }
+
+// marked.use({ extensions: [katexInline, katexBlock] })
 
 const wrapClass = computed(() => {
   return [
