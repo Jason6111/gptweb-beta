7a93adc979c8ba76acff0a2cd04e979ab273062b
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-03-05 14:07:00 +0800

    up

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 import { computed, ref } from 'vue'
 import MarkdownIt from 'markdown-it'
-import hljs from 'highlight.js'
 import mdKatex from '@traptitech/markdown-it-katex'
+import mdhljs from 'markdown-it-highlightjs'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
 
 interface Props {
@@ -16,73 +16,15 @@ const props = defineProps<Props>()
 
 const { isMobile } = useBasicLayout()
 
-const mdi = new MarkdownIt({
-  linkify: true,
-  highlight: (code, language) => {
-    const validLang = !!(language && hljs.getLanguage(language))
-    if (validLang) {
-      const lang = language ?? ''
-      return `<pre><code class="hljs ${language}">${hljs.highlight(code, { language: lang }).value}</code></pre>`
-    }
-    return `<pre style="background: none">${hljs.highlightAuto(code).value}</pre>`
-  },
-})
-
-mdi.use(mdKatex, { blockClass: 'katexmath-block', errorColor: ' #cc0000' })
-
 const textRef = ref<HTMLElement>()
 
-// renderer.html = (html) => {
-//   return `<p>${encodeHTML(html)}</p>`
-// }
+const mdi = new MarkdownIt({
+  linkify: true,
+})
 
-// const katexOptions = {
-//   throwOnError: false,
-// }
-
-// const katexInline = {
-//   name: 'katexInline',
-//   level: 'inline',
-//   start(src: string) {
-//     return src.indexOf('$')
-//   },
-//   tokenizer(src: string) {
-//     const match = src.match(/^\$+([^$\n]+?)\$+/)
-//     if (match) {
-//       return {
-//         type: 'katexInline',
-//         raw: match[0],
-//         text: match[1].trim(),
-//       }
-//     }
-//   },
-//   renderer(token: marked.Tokens.Generic) {
-//     return katex.renderToString(token.text, katexOptions)
-//   },
-// // }
-
-// const katexBlock = {
-//   name: 'katexBlock',
-//   level: 'block',
-//   start(src: string) {
-//     return src.indexOf('\n$$')
-//   },
-//   tokenizer(src: string) {
-//     const match = src.match(/^\$\$+\n([^$]+?)\n\$\$+\n/)
-//     if (match) {
-//       return {
-//         type: 'katexBlock',
-//         raw: match[0],
-//         text: match[1].trim(),
-//       }
-//     }
-//   },
-//   renderer(token: marked.Tokens.Generic) {
-//     return `<p>${katex.renderToString(token.text, katexOptions)}</p>`
-//   },
-// }
-
-// marked.use({ extensions: [katexInline, katexBlock] })
+mdi
+  .use(mdhljs, { auto: true, inline: true })
+  .use(mdKatex, { blockClass: 'katexmath-block rounded-md p-[10px]', errorColor: ' #cc0000' })
 
 const wrapClass = computed(() => {
   return [
@@ -112,7 +54,7 @@ defineExpose({ textRef })
       <span class="dark:text-white w-[4px] h-[20px] block animate-blink" />
     </template>
     <template v-else>
-      <div ref="textRef" v-copy class="leading-relaxed break-words">
+      <div ref="textRef" class="leading-relaxed break-words">
         <div v-if="!inversion" class="markdown-body" v-html="text" />
         <div v-else class="whitespace-pre-wrap" v-text="text" />
       </div>
