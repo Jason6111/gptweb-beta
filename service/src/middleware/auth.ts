06ee41f15960d36be7e18d7de59c9ee2df3de579
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-03-07 22:42:49 +0800

    2.10.0

--- service/src/middleware/auth.ts
+++ service/src/middleware/auth.ts
@@ -2,9 +2,9 @@ const auth = async (req, res, next) => {
   const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
   if (typeof AUTH_SECRET_KEY === 'string' && AUTH_SECRET_KEY.length > 0) {
     try {
-      const token = req.header('Authorization').replace('Bearer ', '')
-      if (token.trim() !== AUTH_SECRET_KEY.trim())
-        throw new Error('Invalid token')
+      const Authorization = req.header('Authorization')
+      if (!Authorization || Authorization.replace('Bearer ', '').trim() !== AUTH_SECRET_KEY.trim())
+        throw new Error('Error: 无访问权限 | No access rights')
       next()
     }
     catch (error) {
--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,5 +1,5 @@
 export * from './app'
-export * from './auth'
 export * from './chat'
 export * from './user'
 export * from './prompt'
+export * from './auth'
